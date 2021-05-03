4fa4dc2036932e9a5cdd0af4022cd39c7256530f
Saravana Pandian <83231084+saravana-pandian@users.noreply.github.com>
2021-05-03 17:31:57 +0530

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -- coding: utf-8 --
+
+{
+    'name' : 'ad_flim',
+    'version' : '14.0.1.0',
+    'summary': ' Flim assign for Actors',
+    'sequence': -100,
+    'description': """Actors Time Managment""",
+    'category': 'Productivity',
+    'website': 'https://www.odoo.com/ad_flim',
+    'license': 'LGPL-3',
+    'depends' : [],
+    'data': [
+        'views/actor.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+
+}
